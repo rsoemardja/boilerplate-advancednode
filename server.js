052b6d39af2f869810dcae574fd2c6d03726ce77
rsoemardja <rsoemardja@gmail.com>
2020-05-30 00:39:13 +1200

    Task 1
    Lets Try

--- server.js
+++ server.js
@@ -1,20 +1,75 @@
-"use strict";
+'use strict';
 
-const express = require("express");
-const fccTesting = require("./freeCodeCamp/fcctesting.js");
-
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const auth = require('./app/auth.js');
+const routes = require('./app/routes.js');
+const mongo = require('mongodb').MongoClient;
+const cookieParser = require('cookie-parser')
+const passport = require('passport');
+const sessionStore = new session.MemoryStore();
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const passportSocketIo = require("passport.socketio");
+
 
 fccTesting(app); //For FCC testing purposes
-app.use("/public", express.static(process.cwd() + "/public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-app.route("/").get((req, res) => {
-  //Change the response to render the Pug template
-  res.send(`Pug template is not defined.`);
-});
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'pug')
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    key: 'express.sid',
+    store: sessionStore,
+}));
+
+
+mongo.connect(process.env.DATABASE, (err, db) => {
+    if (err) console.log('Database error: ' + err);
+
+    auth(app, db);
+    routes(app, db);
+
+    http.listen(process.env.PORT || 3000);
+
+
+    //start socket.io code
+
+    io.use(passportSocketIo.authorize({
+        cookieParser: cookieParser,
+        key: 'express.sid',
+        secret: process.env.SESSION_SECRET,
+        store: sessionStore
+    }));
+
+    var currentUsers = 0;
+
+    io.on('connection', socket => {
+
+        ++currentUsers;
+        console.log('user ' + socket.request.user.name + ' connected');
+        io.emit('user', { name: socket.request.user.name, currentUsers, connected: true });
+
+        socket.on('chat message', (message) => {
+            io.emit('chat message', { name: socket.request.user.name, message });
+        });
+
+        socket.on('disconnect', () => {
+            --currentUsers;
+            io.emit('user', { name: socket.request.user.name, currentUsers, connected: true });
+        });
+
+    });
+
+    //end socket.io code
+
 });
